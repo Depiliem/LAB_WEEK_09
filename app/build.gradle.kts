dba224ad58364628dece041f25a0a80313dee5e9
Depiliem <davew1726@gmail.com>
2025-11-10 21:37:26 +0700

    Assignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,5 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.7.4")
-
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
 }
